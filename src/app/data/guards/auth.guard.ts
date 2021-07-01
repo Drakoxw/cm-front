767722e17dfc822714127a5a39a7115e22e0dd95
unknown <dragon12xw@gmail.com>
2021-06-30 19:00:09 -0500

    antes de ...

--- src/app/data/guards/auth.guard.ts
+++ src/app/data/guards/auth.guard.ts
@@ -1,15 +1,18 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+  autenticacion: boolean;
+
+  constructor(private router: Router) { }
+
+
+  canActivate(): boolean {
     return true;
   }
-  
+
 }
