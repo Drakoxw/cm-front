8c7b83a22eb61de9331463b6e63772d7c8be5ae8
unknown <dragon12xw@gmail.com>
2021-07-01 14:48:44 -0500

    add - lib micro time

--- src/app/data/services/funciones-comunes.service.ts
+++ src/app/data/services/funciones-comunes.service.ts
@@ -5,107 +5,21 @@ import { DomSanitizer } from '@angular/platform-browser';
   providedIn: 'root'
 })
 export class FuncionesComunesService {
-
-
-  visualizar_speech : string = '';/////////
+  visualizar_speech: string = '';/////////
   puerta: number = 0;////////////////////
-  nombre_pros:string;
+  nombre_pros: string;
   ss: string;
   hoy = new Date();
 
-  constructor(private sanitizer: DomSanitizer) {}
+  constructor(private sanitizer: DomSanitizer) { }
 
-  getDatos(){
+  getDatos() {
     let data = [this.nombre_pros, this.ss];
     console.log(data);
     return data;
   }
 
-  getFecha(): string{
-    let mes:any = this.hoy.getMonth()+1;
-    let dia:any = this.hoy.getDate();
-    if (mes < 10) mes = `0${mes}`;
-    if (dia < 10) dia = `0${dia}`;
-    return `${this.hoy.getFullYear()}-${mes}-${dia}`;
-  }
-
-  getHora():string{
-    let min:any = this.hoy.getMinutes();
-    let hora:any = this.hoy.getHours();
-    let sec:any = this.hoy.getSeconds();
-
-    if (min < 10) min = `0${min}`;
-    if (hora < 10) hora = `0${hora}`;
-    if (sec < 10) sec = `0${sec}`;
-
-    return `${hora}:${min}:${sec}`;
-  }
-
-  getHoy():string{
-    return `${this.getFecha()} 00:00:01`;
-  }
-
-  shoterAlert(t:number,h:string):string{
-    const hoy = this.getFecha()
-
-    let min:any = this.hoy.getMinutes();
-    let hora:any = this.hoy.getHours();
-    if (t > min) {
-      min = min + 60;
-      hora = hora - 1;
-    }
-
-    if (min < 10) min = `0${min}`;
-    if (hora < 10) hora = `0${hora}`;
-    min = min - t;
-    const horaAlarma = `${hora}:${min}`
-    return horaAlarma
-  }
-
-  getTomorrow(){
-    let manana =this.toTimeStamp(this.getHoy());
-    manana = manana + 86400000;
-
-    let año:any = this.fromTimeStamp(manana).getFullYear();
-    let mes:any = this.fromTimeStamp(manana).getMonth();
-    let dia:any = this.fromTimeStamp(manana).getDate();
-    let hora:any = this.fromTimeStamp(manana).getMonth();
-    let min:any = this.fromTimeStamp(manana).getMinutes();
-    let sec:any = this.fromTimeStamp(manana).getSeconds();
-
-    mes = mes +1;
-
-    if (mes < 10) mes = `0${mes}`;
-    if (dia < 10) dia = `0${dia}`;
-    if (min < 10) min = `0${min}`;
-    if (hora < 10) hora = `0${hora}`;
-    if (sec < 10) sec = `0${sec}`;
-
-    return `${año}-${mes}-${dia} 00:00:01`;
-  }
-
-  generateHoraAlarma(hora:string, t:number){
-    let horaT =this.toTimeStamp(hora);
-    const tM = t * 60000;
-    const horaAlarma = this.fromTimeStamp(horaT - tM);
-    let h:any = horaAlarma.getHours();
-    let m:any = horaAlarma.getMinutes();
-    if (m < 10) m = `0${m}`;
-    if (h < 10) h = `0${h}`;
-    return `${h}:${m}`;
-  }
-
-  toTimeStamp(time: string):number{
-    let t = new Date(time);
-    return t.getTime();
-  }
-
-  fromTimeStamp(time:number){
-    const date = new Date(time);
-    return date;
-  }
-
-  ordenar(data){
+  ordenar(data) {
     data.sort(function (a, b) {
       if (a.fechahora < b.fechahora) {
         return 1;
@@ -117,7 +31,7 @@ export class FuncionesComunesService {
     });
   }
 
-  ordenar2(data){
+  ordenar2(data) {
     data.sort(function (a, b) {
       if (a.fechahora > b.fechahora) {
         return 1;
@@ -156,13 +70,13 @@ export class FuncionesComunesService {
         return null;
       }
     }
-  );
+    );
 
-  parseJwt(token){ //parseador de tokens
+  parseJwt(token) { //parseador de tokens
     try {
-    return JSON.parse(atob(token.split('.')[1]));
+      return JSON.parse(atob(token.split('.')[1]));
     } catch (e) {
-    return null;
+      return null;
     }
   };
 
