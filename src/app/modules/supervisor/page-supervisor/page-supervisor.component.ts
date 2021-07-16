3d235bbd8e5cb6726b69184ba4b04514ed9fe618
WilDevp <wilmar.garciava@gmail.com>
2021-07-15 21:54:44 -0500

    update supervisor

--- src/app/modules/supervisor/page-supervisor/page-supervisor.component.ts
+++ src/app/modules/supervisor/page-supervisor/page-supervisor.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./page-supervisor.component.css']
 })
 export class PageSupervisorComponent implements OnInit {
+  opcion: number;
 
   constructor() { }
 
