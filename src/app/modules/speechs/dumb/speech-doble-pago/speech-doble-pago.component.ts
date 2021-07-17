9a1d7cd94aa50d49b449bcf61fb0fe8758c5c1fc
Drako <dragon12xw@gmail.com>
2021-07-17 03:03:25 -0500

    a mimir

--- src/app/modules/speechs/dumb/speech-doble-pago/speech-doble-pago.component.ts
+++ src/app/modules/speechs/dumb/speech-doble-pago/speech-doble-pago.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { ClientModel } from 'src/app/data/models/clientes.models';
+import { HttpService } from 'src/app/data/services/http.service';
 
 @Component({
   selector: 'app-speech-doble-pago',
@@ -6,10 +9,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./speech-doble-pago.component.css']
 })
 export class SpeechDoblePagoComponent implements OnInit {
+  cliente$: Partial<ClientModel>;
 
-  constructor() { }
+  constructor(private httpSer: HttpService, private title: Title)
+  {
+    title.setTitle('DOBLE PAGO');
+  }
 
   ngOnInit(): void {
+    this.httpSer.dataClienteE$.subscribe(r => this.cliente$ = r);
   }
 
 }
--- src/app/modules/speechs/speechs-routing.module.ts
+++ src/app/modules/speechs/speechs-routing.module.ts
@@ -2,6 +2,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ROUTES_PATHS } from '../../data/constants/routes/index'
+import { SpeechDoblePagoComponent } from './dumb/speech-doble-pago/speech-doble-pago.component';
 import { PageRanaReneComponent } from './pages/page-rana-rene/page-rana-rene.component';
 import { ServicioClienteComponent } from './pages/servicio-cliente/servicio-cliente.component';
 
@@ -9,6 +10,7 @@ import { ServicioClienteComponent } from './pages/servicio-cliente/servicio-clie
 const routes: Routes = [
   { path: ROUTES_PATHS.SPEECH.RANARENE, component: PageRanaReneComponent},
   { path: ROUTES_PATHS.SPEECH.SERVICE, component: ServicioClienteComponent},
+  { path: 'doblepago', component: SpeechDoblePagoComponent},
 ];
 
 @NgModule({
