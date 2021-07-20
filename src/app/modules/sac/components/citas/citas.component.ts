05e33316c9a4a6410e13c661878d815db387ad43
Drakoxw <dragon12xw@gmail.com>
2021-07-19 21:52:05 -0500

    salida

--- src/app/modules/sac/components/citas/citas.component.ts
+++ src/app/modules/sac/components/citas/citas.component.ts
@@ -1,4 +1,8 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { RespCitas } from 'src/app/data/models/respuestas.models';
+import { HttpService } from 'src/app/data/services/http.service';
+import { MicroTimeService } from 'src/app/data/services/micro-time.service';
 
 @Component({
   selector: 'app-citas',
@@ -6,12 +10,52 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
   styleUrls: ['./citas.component.css']
 })
 export class CitasComponent implements OnInit {
-  @Output() tituloEmit = new EventEmitter<string>()
+  dataCitas: RespCitas[];
+  hoy: string;
+  idCliente: string;
 
-  constructor() { }
+
+
+  // data
+  fecha_cita: string;
+  hora_cita: string;
+  asignado: string;
+  estado_cita: string = '1';
+  nombre: string;
+  motivo_cita: string;
+  nota_cita: string;
+  id_asignado: string;
+
+
+
+  constructor(private timer: MicroTimeService,
+              private route: ActivatedRoute,
+              private httpSer: HttpService)
+  {
+    this.hoy = `${this.timer.getFecha()}`;
+    this.idCliente = this.route.snapshot.params['id'];
+  }
 
   ngOnInit(): void {
-
+    this.getCitas();
   }
+
+  getCitas(){
+    let id = this.idCliente;
+    this.httpSer.getAllCitasId(id).subscribe(r => {
+      if (!r.error) {
+        this.dataCitas = [];
+        for (const key in r.data){
+          const el = r.data[key];
+          this.dataCitas.push(el)
+        }
+      }
+    });
+  }
+
+
+
+  call(item){}
+  agregarCita(){}
 
 }
